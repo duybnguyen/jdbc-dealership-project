02918f17b014137821371a0c160f33d6c42009f8
duy nguyen <duybnguyen04@gmail.com>
2025-12-08 09:21:23 -0500

    Implement addLeaseContract: - Accepts a LeaseContract object as input - Inserts a new lease contract into the sales_contracts table - Uses a prepared statement to set VIN, lease_start, lease_end, and monthly_payment

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -4,6 +4,7 @@ import com.yearup.dealership.models.LeaseContract;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -15,6 +16,22 @@ public class LeaseDao {
     }
 
     public void addLeaseContract(LeaseContract leaseContract) {
-        // TODO: Implement the logic to add a lease contract
+        String sql = "INSERT INTO lease_contracts (vin, lease_start, lease_end, monthly_payment) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setString(1, leaseContract.getVin());
+            statement.setDate(2, Date.valueOf(leaseContract.getLeaseStart()));
+            statement.setDate(3, Date.valueOf(leaseContract.getLeaseEnd()));
+            statement.setDouble(4, leaseContract.getMonthlyPayment());
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error adding lease contract");
+            e.printStackTrace();
+        }
     }
+
 }
