ffaa3771b1dc5882470f8076931df731df86c9d4
duy nguyen <duybnguyen04@gmail.com>
2025-12-08 20:06:06 -0500

    Implement addVehicleToInventory & removeVehicleFromInventory: - to add a vehicle we insert vin and dealership_id into inventory table. - to remove we search for vehicle with matching vin in database and execute DELETE.

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -13,10 +13,33 @@ public class InventoryDao {
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+        String sql = "INSERT INTO inventory (vin, dealership_id) VALUES (?, ?)";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setString(1, vin);
+            statement.setInt(2, dealershipId);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error adding vehicle to inventory");
+            e.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
-        // TODO: Implement the logic to remove a vehicle from the inventory
+        String sql = "DELETE FROM inventory WHERE vin = ?";
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setString(1, vin);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error removing vehicle from inventory");
+            e.printStackTrace();
+        }
     }
 }
